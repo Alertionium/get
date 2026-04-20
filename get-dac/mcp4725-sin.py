0f947889a6c72c0b0816116559e8ef5a19d9e7e8
Alertionium <grishasterlikov@gmail.com>
2026-04-20 11:50:39 +0300

    mcp4725 синусоида

---
+++ get-dac/mcp4725-sin.py
@@ -0,0 +1,21 @@
+import mcp4725_driver as mcp
+import signal_generator as sg
+import time
+amplitude= 3.2
+signal_frequency= 10
+sampling_frequency=1000
+
+try:
+    dac =mcp.MCP4725(3.3, 0x61, False)
+
+    start_time=time.time()
+
+    while True:
+        current_time=time.time()-start_time
+        normalized_amp = sg.get_sin_wave_amplitude(signal_frequency, current_time)
+        voltage=normalized_amp*amplitude
+        dac.set_voltage(voltage)
+        sg.wait_for_sampling_period(sampling_frequency)
+
+finally:
+    dac.deinit()
